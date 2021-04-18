29ced88301227d1c57a7c32eb3d8567f7109a906
Xian-En Lin <blueeye851114@gmail.com>
2021-04-18 11:22:10 +0800

    升序降序 modal 完成

--- country.js
+++ country.js
@@ -1,11 +1,15 @@
 let app = new Vue({
   el: '#table',
   data: {
-    allData: null,
-    showData: [],
+    searchText: '',
+    searcher: null,
+    dataAll: [],
+    dataShow: [],
+    dataDetail: {},
     currentPage: 1,
     countPerPage: 25,
-    
+    isAsc: true,
+    bsModal: new bootstrap.Modal(document.querySelector('#modal-detail')),
   },
   methods: {
     sortAsc(data) {
@@ -14,27 +18,59 @@ let app = new Vue({
         let name2 = country2.name.toLowerCase();
         return name1 > name2 ? 1 : -1;
       });
+    },
+    updatedataShow() {
+      this.dataShow = this.dataAll.slice(this.countPerPage * (this.currentPage - 1), this.countPerPage * this.currentPage);
+    },
+    showDetail(data) {
+      this.dataDetail = data;
+    },
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.dataAll.length / this.countPerPage);
+    },
+    dataDetailText() {
+      let copy = Object.assign({}, this.dataDetail);
+      delete copy.name;
+      delete copy.flag;
+      let arr = Object.entries(copy).sort();
+      return arr;
     }
   },
+  watch: {
+    isAsc: function() {
+      this.dataAll = this.dataAll.reverse();
+      this.updatedataShow();
+    },
+    currentPage: function() {
+      this.updatedataShow();
+    },
+  },
   created: function() {
-    let initFields = [
-      'flag',
-      'name',
-      'alpha2Code',
-      'alpha3Code',
-      'nativeName',
-      'altSpellings',
-      'callingCodes'
-    ];
-    axios.get(`https://restcountries.eu/rest/v2/all?fields=${initFields.join(';')}`)
+    axios.get(`https://restcountries.eu/rest/v2/all`)
       .then(res => {
-        this.allData = this.sortAsc(res.data);
-        this.showData = this.allData.slice(this.countPerPage * (this.currentPage - 1), this.countPerPage * this.currentPage);
+        this.dataAll = this.sortAsc(res.data);
+        this.updatedataShow();
         
-        // app.allData.forEach((item) => {
-        //   console.log(item.name);
-        // });
-        // console.log(res);
+        this.searcher = new FuzzySearch(people, ['name.firstName', 'state'], {
+          caseSensitive: true,
+        });
+        
+        // console.log(res.data);
       });
   }
 });
+
+const people = [{
+  name: {
+    firstName: 'Jesse',
+    lastName: 'Bowen',
+  },
+  state: 'Seattle',
+}];
+const searcher = new FuzzySearch(people, ['name.firstName', 'state'], {
+  caseSensitive: true,
+});
+const result = searcher.search('ess');
+console.log(result);
